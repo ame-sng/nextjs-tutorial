7b54ed048922a2aa3db78e1c3f7dbed8dd52fe7d
“ame-sng” <ameliasnghy@gmail.com>
2021-11-18 16:55:31 +0800

    tutorial 5 done

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,12 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Homepage - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent quis magna molestie, congue ipsum sed, interdum orci. Nulla elementum ante eget dictum porta. Vivamus eu faucibus neque, vel egestas massa. Donec sapien augue, rhoncus ornare augue quis, fringilla porttitor lacus.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent quis magna molestie, congue ipsum sed, interdum orci. Nulla elementum ante eget dictum porta. Vivamus eu faucibus neque, vel egestas massa. Donec sapien augue, rhoncus ornare augue quis, fringilla porttitor lacus.</p>
       <Link href="/listing">
         <a>See All Listings</a>
       </Link>
-      <Footer />
     </div>
   )
 }
