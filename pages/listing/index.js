d524f3224a9a8b1c7f9790594e20d3c66a9a92a9
“ame-sng” <ameliasnghy@gmail.com>
2021-11-19 13:46:02 +0800

    tutorial 10 done

--- pages/listing/index.js
+++ pages/listing/index.js
@@ -1,7 +1,30 @@
-const Listing = () => {
+import styles from '../../styles/Listing.module.css'
+
+export const getStaticProps = async() => { //similar to useEffect. Runs at build time, as our app is built and component renders
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json();
+
+  return {
+    props: {
+      listings: data
+    }
+  }
+}
+
+const Listing = ({listings}) => {
   return (
     <div>
       <h1>All Listings</h1>
+      {listings.map((user) => (
+        <div key={user.id}>
+          <a className={styles.single}>
+            <h3>
+              {user.name}
+            </h3>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
